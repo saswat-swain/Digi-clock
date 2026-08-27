579ec1996342cbb32a4ec5c4c2baacc7f5cf9a3b
saswat-swain <swainsaswat308@gmail.com>
2026-08-27 20:48:55 +0530

    Digi clock

---
+++ main.js
@@ -0,0 +1,42 @@
+let clock = document.getElementById("clock");
+
+function showTime(){
+
+    let now = new Date();
+
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    let period = "AM";
+
+    if(hour >= 12){
+        period = "PM";
+    }
+
+    if(hour > 12){
+        hour = hour - 12;
+    }
+
+    if(hour == 0){
+        hour = 12;
+    }
+
+    if(hour < 10){
+        hour = "0" + hour;
+    }
+
+    if(minute < 10){
+        minute = "0" + minute;
+    }
+
+    if(second < 10){
+        second = "0" + second;
+    }
+
+    clock.innerHTML = hour + ":" + minute + ":" + second + " " + period;
+}
+
+showTime();
+
+setInterval(showTime, 1000);
